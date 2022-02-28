858c60ca8de3c88c8acb039618c3b5c7b1733ade
Rusta007 <jp9016161598@gmail.com>
2022-02-28 01:03:21 -1200

    react-2

--- src/React Assignment 1/App.jsx
+++ src/React Assignment 1/App.jsx
@@ -12,3 +12,14 @@ function App(){
 
 
 export default App;
+
+
+
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from './React Assignment 1/App';
+
+
+// ReactDOM.render(<><App /></>,document.getElementById('root'));
+
